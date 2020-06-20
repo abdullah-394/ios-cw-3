5ded58ca92f1f174e3bd545a5e665c32edd6c665
abdullah-394 <alenezi.abdullah2003@gmail.com>
2020-06-20 20:46:40 +0300

    done

---
+++ io-cw3-p1.playground/Contents.swift
@@ -0,0 +1,10 @@
+var number1 = 5
+var number2 = 3
+
+func add( a: Int , b: Int ) ->Int
+    
+{
+    return a + b
+}
+var functionCall = add(a: number1, b:number2)
+print("  \( number1 ) + \( number2 ) = \(functionCall)" )
